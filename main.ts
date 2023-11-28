4b5bfa32fa076ec48e5f485a0b1ef44ea88b50c4
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-11-28 12:31:08 -0500

    zdfhsdfs

--- main.ts
+++ main.ts
@@ -2,7 +2,35 @@
  *
  * Created by: Sophie
  * Created on: Nov 2023
- * This program ...
+ * This program turns a 2 - 28BYJ-48 DC 5V until a sonar sensor senses an object 10cm away
 */
 
-basic.showString('Hello, World!')
+let distanceToObject: number = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+while (true) {
+  if (input.buttonIsPressed(Button.A) === true) {
+    // find distance from sonar
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+      DigitalPin.P1,
+      DigitalPin.P2,
+      PingUnit.Centimeters
+    )
+    basic.showNumber(distanceToObject)
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+
+    if (distanceToObject < 10) {
+      robotbit.StpCarMove(-10, 48)
+      basic.pause(500)
+      robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+      basic.pause(500)
+      robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
+      basic.pause(500)
+      robotbit.StpCarMove(10, 48)
+    }
+  }
+}
