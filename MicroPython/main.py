38d37b91d221f5a1509eee7f63243aee0f317823
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-11-29 18:25:11 +0000

    py

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,11 @@
 """
 Created by: Mr. Coxall
 Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+This module is a Micro:bit MicroPython program 
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.show(Image.HEART)
+
