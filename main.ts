210ecd268a3caf2af3c674c930e5110297e355a9
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-11-29 12:29:37 -0500

    indent

--- main.ts
+++ main.ts
@@ -17,23 +17,23 @@ while (true) {
       // find distance from sonar
       basic.clearScreen()
       distanceToObject = sonar.ping(
-          DigitalPin.P1,
-          DigitalPin.P2,
-          PingUnit.Centimeters
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
       )
 
       // turn if distance < 10
       if (distanceToObject < 10) {
-          robotbit.StpCarMove(-10, 48)
-          basic.pause(500)
-          robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
-          basic.pause(500)
-          robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
-          basic.pause(500)
-          robotbit.StpCarMove(10, 48)
+        robotbit.StpCarMove(-10, 48)
+        basic.pause(500)
+        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+        basic.pause(500)
+        robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
+        basic.pause(500)
+        robotbit.StpCarMove(10, 48)
       } else {
-          // move forward
-          robotbit.StpCarMove(10, 48)
+        // move forward
+        robotbit.StpCarMove(10, 48)
       }
     }
   }
